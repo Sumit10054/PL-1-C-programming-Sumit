350562179f0d3c113364ed815e692c8168190b39
Sumit10054 <sumitmahajan1005@gmail.com>
2026-09-20 17:33:50 +0530

    Refactor average calculation with predefined values

--- sdm5.c
+++ sdm5.c
@@ -1,23 +1,17 @@
 /*
-Program(6)- Write a program to calculate the average of three numbers.
-Solution(2)-User Define Values.
+Program(5)- Write a program to calculate the average of three numbers.
+Solution(1)- Predefined Values.
 */
 
 #include<stdio.h>
 int main()
 {
-int num1,num2,num3,total;
+int a=33;
+int b=47;
+int c=20;
 float average;
 
-printf("Enter first number:");
-scanf("%d",&num1);
-printf("Enter second number:");
-scanf("%d",&num2);
-printf("Enter third number:");
-scanf("%d",&num3);
-
-total=(num1+num2+num3);
-average=total/3;
+average=(a+b+c)/3;
 printf("\n Average=%f",average);
 
 return 0;
